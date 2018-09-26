ce0f16c9bd25c418dbca116fdff6aefa13ffcaa9
Jayden Seric <me@jaydenseric.com>
2018-09-26 19:25:28 +1000

    Simpler _app.js.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,43 @@
-import { Provider as GraphQLProvider } from 'graphql-react'
+import 'cross-fetch/polyfill'
+import { GraphQL, preload, Provider as GraphQLProvider } from 'graphql-react'
 import App, { Container } from 'next/app'
-import withGraphQL from '../withGraphQL'
+import Head from 'next/head'
 
-class GraphQLReactExampleApp extends App {
+export default class CustomApp extends App {
+  static async getInitialProps({ ctx, router, Component }) {
+    const props = {}
+
+    if (Component.getInitialProps)
+      props.pageProps = await Component.getInitialProps(ctx)
+
+    if (ctx.req) {
+      const graphql = new GraphQL()
+      await preload(
+        <CustomApp
+          {...props}
+          graphql={graphql}
+          router={router}
+          Component={Component}
+        />
+      )
+      Head.rewind()
+      props.graphqlCache = graphql.cache
+    }
+
+    return props
+  }
+
+  graphql =
+    this.props.graphql || new GraphQL({ cache: this.props.graphqlCache })
+
   render() {
-    const { Component, pageProps, graphql } = this.props
+    const { Component, pageProps } = this.props
     return (
       <Container>
-        <GraphQLProvider value={graphql}>
+        <GraphQLProvider value={this.graphql}>
           <Component {...pageProps} />
         </GraphQLProvider>
       </Container>
     )
   }
 }
-
-export default withGraphQL(GraphQLReactExampleApp)
