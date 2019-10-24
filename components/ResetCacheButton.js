4c7fc9bafe9547e6965a8664bf67af951f9a2bf1
Jayden Seric <me@jaydenseric.com>
2019-10-25 10:49:07 +1100

    Use React.useContext hook.

--- components/ResetCacheButton.js
+++ components/ResetCacheButton.js
@@ -1,9 +1,9 @@
 import { GraphQLContext } from 'graphql-react'
+import React from 'react'
 
-const ResetCacheButton = () => (
-  <GraphQLContext.Consumer>
-    {graphql => <button onClick={graphql.reset}>Reset cache</button>}
-  </GraphQLContext.Consumer>
-)
+const ResetCacheButton = () => {
+  const graphql = React.useContext(GraphQLContext)
+  return <button onClick={graphql.reset}>Reset cache</button>
+}
 
 export default ResetCacheButton
