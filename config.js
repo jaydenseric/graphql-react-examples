761efe13de3db7c153c64f4901008bcb88d52cf9
Jayden Seric <me@jaydenseric.com>
2020-09-17 11:20:07 +1000

    Temporarily change the GraphQL Pokemon API URL.
    See https://github.com/lucasbento/graphql-pokemon/pull/14 .

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 export const pokemonFetchOptionsOverride = (options) => {
-  options.url = 'https://graphql-pokemon.now.sh';
+  // Temporary URL, see:
+  // https://github.com/lucasbento/graphql-pokemon/pull/14
+  options.url = 'https://graphql-pokemon2.vercel.app';
 };
 
 export const graphqlHubFetchOptionsOverride = (options) => {
