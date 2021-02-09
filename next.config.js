3ba63a505ca9ae5405f2fe83647840627d05c0b8
Jayden Seric <me@jaydenseric.com>
2021-02-09 22:14:28 +1100

    Better locale system.

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
     ORIGIN: process.env.ORIGIN || `https://${process.env.VERCEL_URL}`,
     GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
   },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
 };
